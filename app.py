8c8cf49d1fd8facbadafc3224aeb39b9c178f5cb
8Emiliano12 <144985336+8Emiliano12@users.noreply.github.com>
2026-09-21 13:15:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ if not check_password():
 # ==========================================
 st.title("Panel de Control: Rendimiento y Bienestar - Pumas CU")
 
-# 1. Base de datos con el Roster Completo Precargado
+# Roster Base por si la nube está vacía
 roster_data = [
     (0, "Jioshi Alexander Morrison González", "DL"),
     (1, "Raúl Rodrigo Blanco Ruiz", "WR"),
@@ -160,12 +160,12 @@ datos_iniciales = {
     'Historial_Rendimiento_Juego': [[0.0]]*n_jugadores
 }
 
-# --- CARGA SEGURA DESDE GOOGLE SHEETS CON RESPALDO LOCAL ---
+# --- CONEXIÓN Y CARGA DESDE GOOGLE SHEETS ---
 if 'df' not in st.session_state:
     try:
         conn = st.connection("gsheets", type="gsheets")
         df_cloud = conn.read(worksheet="Sheet1", ttl=0)
-        if df_cloud is not None and not df_cloud.empty and 'Jugador' in df_cloud.columns:
+        if not df_cloud.empty and 'Jugador' in df_cloud.columns:
             st.session_state.df = df_cloud
         else:
             st.session_state.df = pd.DataFrame(datos_iniciales)
